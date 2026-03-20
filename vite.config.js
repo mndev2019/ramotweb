87578ebc7d7310e17c424c6bffc4dc0f79737f57
mndev2019 <muskanvision2019@gmail.com>
2026-03-20 12:48:14 +0530

    `Update vite.config.js: remove commented out code and unused import`

--- vite.config.js
+++ vite.config.js
@@ -7,28 +7,28 @@
 
 
 
-// import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
-// import react from '@vitejs/plugin-react-swc'
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: 'react',
-      plugins: [],         // ✅ disables refresh plugins
-    }),
-  ],
-  server: {
-    hmr: false,           // 🔥 FULLY DISABLE HMR (MAIN FIX)
-  },
+  plugins: [react()],
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       jsxImportSource: 'react',
+//       plugins: [],         // ✅ disables refresh plugins
+//     }),
+//   ],
+//   server: {
+//     hmr: false,           // 🔥 FULLY DISABLE HMR (MAIN FIX)
+//   },
+// })
 
 
